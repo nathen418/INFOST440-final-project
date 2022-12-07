d72116f426170f4ac2857f7fdb777a0c2a7fb7d8
Nate Goldsborough <nathen418@playantares.com>
2022-12-07 09:45:32 -0600

    fix password page

--- password.php
+++ password.php
@@ -95,12 +95,54 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
 } // End of the main Submit conditional.
 ?>
-<h1>Change Your Password</h1>
-<form action="password.php" method="post">
-	<p>Email Address: <input type="text" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"  /> </p>
-	<p>Current Password: <input type="password" name="pass" size="10" maxlength="20" value="<?php if (isset($_POST['pass'])) echo $_POST['pass']; ?>"  /></p>
-	<p>New Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>"  /></p>
-	<p>Confirm New Password: <input type="password" name="pass2" size="10" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>"  /></p>
-	<p><input type="submit" name="submit" value="Change Password" /></p>
+<form action="register.php" method="post">
+	<section class="vh-100">
+		<div class="container py-5 h-100">
+			<div class="row d-flex justify-content-center align-items-center h-100">
+				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
+					<div class="card bg-dark text-white" style="border-radius: 1rem;">
+						<div class="card-body p-5 text-center">
+							<div class="mb-md-5 mt-md-4 pb-5">
+								<h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+								<p class="text-white-50 mb-5">Please enter the required information to change your password</p>
+
+								<div class="form-outline form-white mb-4">
+									<input type="text" name="email" class="form-control form-control-lg" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+									<label class="form-label" for="first_name">Email</label>
+								</div>
+
+								<div class="form-outline form-white mb-4">
+									<input type="password" name="pass1" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>">
+									<label class="form-label" for="first_name">Password</label>
+								</div>
+
+								<div class="form-outline form-white mb-4">
+									<input type="password" name="pass2" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>">
+									<label class="form-label " for="first_name">Confirm Password</label>
+								</div>
+
+								<button class="btn btn-outline-light btn-lg px-5" type="submit">Change Password</button>
+								<?php
+								if ($errors) {
+									echo '<p class="error">The following error(s) occurred:<br />';
+									foreach ($errors as $msg) { // Print each error.
+										echo " - $msg<br />\n";
+									}
+									echo '</p><p>Please try again.</p><p><br /></p>';
+
+									mysqli_close($dbc);
+									exit();
+								}
+								?>
+							</div>
+							<div>
+								<p class="mb-0">Already have an account?<a href="login.php" class="text-white-50 fw-bold">Log In</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </form>
 <?php include ('footer.php'); ?>
