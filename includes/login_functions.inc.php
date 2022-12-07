1a1c27b5afba37d6a311e4891efcd416ca22bb02
Flakari <natdimock@yahoo.com>
2022-12-07 10:52:57 -0600

    Change http header to https

--- includes/login_functions.inc.php
+++ includes/login_functions.inc.php
@@ -8,8 +8,8 @@
 function redirect_user ($page = 'index.php') {
 
 	// Start defining the URL...
-	// URL is http:// plus the host name plus the current directory:
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	// URL is https:// plus the host name plus the current directory:
+	$url = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
 	
 	// Remove any trailing slashes:
 	$url = rtrim($url, '/\\');
