b3e0df06658066bb60733f7d7a4d057466f58b38
Nate Goldsborough <nathen418@playantares.com>
2022-12-07 09:54:15 -0600

    add fields and fix references

--- password.php
+++ password.php
@@ -3,13 +3,12 @@
 
 $page_title = 'Change Your Password';
 include ('header.php');
+$errors = array();
 
 // Check for form submission:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	require ('mysqli_connect.php'); // Connect to the db.
-		
-	$errors = array(); // Initialize an error array.
 	
 	// Check for an email address:
 	if (empty($_POST['email'])) {
@@ -95,7 +94,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
 } // End of the main Submit conditional.
 ?>
-<form action="register.php" method="post">
+<form action="password.php" method="post">
 	<section class="vh-100">
 		<div class="container py-5 h-100">
 			<div class="row d-flex justify-content-center align-items-center h-100">
@@ -103,7 +102,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 					<div class="card bg-dark text-white" style="border-radius: 1rem;">
 						<div class="card-body p-5 text-center">
 							<div class="mb-md-5 mt-md-4 pb-5">
-								<h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+								<h2 class="fw-bold mb-2 text-uppercase">Change Password</h2>
 								<p class="text-white-50 mb-5">Please enter the required information to change your password</p>
 
 								<div class="form-outline form-white mb-4">
@@ -112,13 +111,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 								</div>
 
 								<div class="form-outline form-white mb-4">
+									<input type="password" name="pass" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['pass'])) echo $_POST['pass']; ?>">
+									<label class="form-label" for="pass">Old Password</label>
+								</div>
+
+								<div class="form-outline form-white mb-4">
 									<input type="password" name="pass1" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>">
-									<label class="form-label" for="first_name">Password</label>
+									<label class="form-label" for="pass1">New Password</label>
 								</div>
 
 								<div class="form-outline form-white mb-4">
 									<input type="password" name="pass2" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>">
-									<label class="form-label " for="first_name">Confirm Password</label>
+									<label class="form-label" for="pass2">Confirm Password</label>
 								</div>
 
 								<button class="btn btn-outline-light btn-lg px-5" type="submit">Change Password</button>
