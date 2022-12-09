6aeb9a34d9786af3fd4c8d3326452a9f9897a4d6
Nate Goldsborough <nathen418@playantares.com>
2022-12-09 12:29:26 -0600

    Style createblogpost after register.

--- createblogpost.php
+++ createblogpost.php
@@ -1,80 +1,72 @@
 <?php
-    // Needed imports to make guestbook work
-    include('header.php');
-    include('mysqli_connect.php');
-?>
+// Needed imports to make guestbook work
+$page_title = "Make a Post";
+include('header.php');
+$errors = array();
+// Check for form submission:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-<?php
-    // Values from comment form
-    $fname = isset($_REQUEST['fname']) ? mysqli_real_escape_string($dbc, trim($_POST['fname'])) : '';
-    $lname = isset($_REQUEST['lname']) ? mysqli_real_escape_string($dbc, trim($_POST['lname'])) : '';
-    $comment = isset($_REQUEST['comment']) ? mysqli_real_escape_string($dbc, trim($_POST['comment'])) : '';
+	require('mysqli_connect.php'); // Connect to the db.
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Form validation
-        $valid_form = true;
+	// Check for a first name:
+	if (empty($_POST['title'])) {
+		$errors[] = 'You forgot to enter a title for your post.';
+	} else {
+		$fname = mysqli_real_escape_string($dbc, trim($_POST['title']));
+	}
 
-        if ($fname == '') {
-            $valid_form = false;
-            echo "<p>Please fill out 'First Name' field!</p>";
-        }
-
-        if ($lname == '') {
-            $valid_form = false;
-            echo "<p>Please fill out 'Last Name' field!</p>";
-        }
-
-        if ($comment == '') {
-            $valid_form = false;
-            echo "<p>Please leave a comment!</p>";
-        }
-
-        // Only if form is valid, then send query to MySQL database
-        if ($valid_form) {
-            $query = "INSERT INTO guestbook (fname, lname, comment) VALUES ('$fname', '$lname', '$comment')";
-            $results = mysqli_query($dbc, $query);
-
-            if ($results) {
-                echo "It worked! The SQL query was run!";
-            } else {
-                echo "There was an error: " . mysqli_error($dbc);
-            }
-        }
+	// Check for a last name:
+	if (empty($_POST['body'])) {
+		$errors[] = 'You forgot to enter a body for your post.';
+	} else {
+		$lname = mysqli_real_escape_string($dbc, trim($_POST['body']));
+	}
+    if (!$errors[0]){
+        echo "<script> window.location.assign('index.php'); </script>";
     }
+}
 ?>
 <br />
-<form action="comment.php" method="post">
-    <fieldset>
-        <legend>Please enter your guestbook entry:</legend>
-        <p>
-            <label for="fname">First Name: </label>
-            <input type="text" name="fname" id="fname" value="<?php
-                if (isset($fname)) {
-                    echo stripcslashes($fname);
-                }
-            ?>">
-        </p>
-        <p>
-            <label for="lname">Last Name: </label>
-            <input type="text" name="lname" id="lname" value="<?php
-                if (isset($lname)) {
-                    echo stripcslashes($lname);
-                }
-            ?>">
-        </p>
-        <p>
-            <label for="comment">Please enter a comment: </label><br />
-            <textarea name="comment" id="comment" cols="40" rows="5"><?php
-                if (isset($comment)) {
-                    echo stripcslashes($comment);
-                }
-            ?></textarea>
-        </p>
+<form action="createblogpost.php" method="POST">
+    <section class="vh-100">
+        <div class="container py-5">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+                            <h2 class="fw-bold mb-2 text-uppercase">Write a Blog Post</h2>
+                            <p class="text-white-50">Please fill out the required information to post</p>
 
-        <input type="submit" value="Submit">
-    </fieldset>
+                            <div class="form-outline form-white mb-4">
+                                <label class="form-label" for="title">Title</label>
+                                <input type="title" name="title" class="form-control form-control-lg" maxlength="128" value="<?php if (isset($_POST['title'])) echo $_POST['title']; ?>">
+                            </div>
 
+                            <div class="form-outline form-white mb-4">
+                                <label class="form-label" for="body">Post Body</label>
+                                <textarea name="body" class="form-control form-control-lg" maxlength="20" value="<?php if (isset($_POST['body'])) echo $_POST['body']; ?>"></textarea>
+                            </div>
+
+                            <button class="btn btn-outline-light btn-lg px-5" type="submit">Post</button>
+                            <?php
+                            if ($errors) {
+                                echo '<p class="error">The following error(s) occurred:<br />';
+                                foreach ($errors as $msg) { // Print each error.
+                                    echo " - $msg<br />\n";
+                                }
+                                echo '</p><p>Please try again.</p><p><br /></p>';
+
+                                mysqli_close($dbc);
+                                exit();
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </form>
 <?php
-    include('footer.php');
+include('footer.php');
 ?>
