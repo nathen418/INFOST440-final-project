12c0259ca9328ebd7bc21a6816781b365bc0dbb2
Nate Goldsborough <nathen418@playantares.com>
2022-12-10 15:26:46 -0600

    Replace navbar, remove unused styling

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 	<meta name="keywords" content="Nathen, Nathan">
 	<meta name="author" content="Nathen and Nathan">
 	<meta property="og:type" content="website">
-	<meta property="og:title" content="440 Blog">
+	<meta property="og:title" content="bLog">
 	<meta property="og:description" content="The badly designed blog">
 	<meta property="theme-color" content="#6761A8">
 
@@ -22,56 +22,12 @@
 			text-align: center;
 		}
 
-		.header {
-			background-color: #dbd1c4;
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			padding: 5px 20px;
-			border-radius: 5px 5px 0 0;
-		}
-
-		.header h1 {
-			font-size: 1.4em;
-			margin-right: 20px;
-		}
-
-		.header p {
-			font-weight: bold;
-		}
-
 		.content {
 			padding: 5px 20px;
 		}
 
-		/* Nav bar  */
-		#navigation {
-			display: flex;
-			justify-content: right;
-		}
-
-
-		#navigation ul {
-			list-style-type: none;
-			margin: 0;
-			padding: 0;
-			overflow: hidden;
-		}
-
-		#navigation li {
-			float: left;
-		}
-
-		#navigation li a {
-			display: block;
-			padding: 8px;
-			background-color: #dddddd;
-		}
-
 		body {
-			/* fallback for old browsers */
 			background: #6a11cb;
-			/* Chrome 10-25, Safari 5.1-6 */
 			background: -webkit-linear-gradient(right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
 		}
 
@@ -86,32 +42,51 @@
 </head>
 
 <body>
-	<div id="navigation">
-		<ul>
-			<li><a href="index.php">Home Page</a></li>
-			<li><a href="register.php">Register</a></li>
-			<?php
-			if (isset($_SESSION['user_id']) && $_SESSION['is_admin'] == 1) {
-			?>
-				<li><a href="adminpanel.php">Admin Panel</a></li>
-				<li><a href="view_users.php">View Users</a></li>
-				<li><a href="password.php">Change Password</a></li>
-
-			<?php
-			}
-			?>
-			<?php
-			// Create a login/logout link:
-			if ((isset($_SESSION['user_id'])) && (basename($_SERVER['PHP_SELF']) != 'logout.php')) {
-			?>
-				<li><a href="logout.php">Logout</a></li>
-			<?php
-			} else {
-			?>
-				<li><a href="login.php">Login</a></li>
-			<?php
-			}
-			?>
-		</ul>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded-bottom ">
+		<div class="container-fluid">
+			<a class="navbar-brand mt-2 mt-lg-0" href="#">
+				<img src="resources/logo.png" height="32" alt="bLog Logo" loading="lazy" />
+			</a>
+			<a class="navbar-brand" href="index.php">bLog</a>
+			<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fas fa-bars"></i>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="index.php">Home</a>
+					</li>
+					<?php
+					if (isset($_SESSION['user_id']) && $_SESSION['is_admin'] == 1) {
+					?>
+						<li class="nav-item">
+							<a class="nav-link" href="post.php">Post</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="admin.php">Admin Panel</a>
+						</li>
+					<?php }
+					// Create a login/logout link:
+					if ((isset($_SESSION['user_id'])) && (basename($_SERVER['PHP_SELF']) != 'logout.php')) { ?>
+						<li class="nav-item">
+							<a class="nav-link" href="logout.php">Logout</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="password.php">Change Password</a>
+						</li>
+					<?php } else { ?>
+						<li class="nav-item">
+							<a class="nav-link" href="login.php">Login</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="register.php">Register</a>
+						</li>
+					<?php } ?>
+					<li class="nav-item">
+						<a class="nav-link" href=""><?php echo @$_SESSION['first_name']; ?></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div id="content">
